ea527827adafebc053c72317baa79a29e876cbe7
otiscrop <otiscrop@gmail.com>
2015-09-01 13:41:00 +0200

    -Updating string

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -1,9 +1,11 @@
 package h01;
 
+//V2.0
+
 public class HalloRocDev {
 
 	public static void main(String[] args) {
-		System.out.println("Hallo Yuliya!");
+		System.out.println("Hallo Roc-dev!");
 	}
 
 }
